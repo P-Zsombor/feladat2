a85908984e04d6aa37c61cc1420ef3416d545f36
potyondi.zsombor <potyondi.zsombor@C19-13>
2025-03-31 13:31:10 +0200

    Add project files.

---
+++ feladat/Connection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace feladat
+{
+    class Connection
+    {
+        HttpClient client = new HttpClient();
+        string serverUrl = "";
+
+        public Connection(string serverUrl)
+        {
+            this.serverUrl = serverUrl;
+        }
+
+        public async void Create(string name, float score, int price)
+        {
+            string url = serverUrl + "/createKolbasz";
+
+            try
+            {
+                var jsonInfo = new
+                {
+                    name = name,
+                    score = score,
+                    price = price
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent send = new StringContent(jsonStringified, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, send);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine("Sikeres létrehozás");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public async Task<List<string>> All()
+        {
+            string url = serverUrl + "/kolbaszok";
+            List<string> list = new List<string>();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                list = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => $"{item.name},{item.score},{item.price}").ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return list;
+        }
+
+        public async void Delete(int id)
+        {
+            string url = $"{serverUrl}/deleteKolbasz/{id}";
+
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, url);
+                HttpResponseMessage response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine("Sikeres törlés");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ feladat/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat
+{
+    class Program
+    {
+        static Connection connection;
+        static void Main(string[] args)
+        {
+            Start();
+        }
+        static void Start()
+        {
+            connection = new Connection("http://127.1.1.1:3000");
+
+            inp();
+        }
+        static async void inp()
+        {
+            Console.WriteLine("Mit szeretnél csinálni? (vásárolni, nézelődni, törölni)");
+            string input = Console.ReadLine();
+            if (input == "q")
+            {
+
+            }
+            else if (input == "")
+            {
+                Console.Clear();
+                inp();
+            }
+            else
+            {
+                Load(input);
+                inp();
+            }
+        }
+        static async void Load(string input)
+        {
+            if (input == "vásárolni" || input == "1")
+            {
+                Console.WriteLine("Mi legyen a neve?");
+                string name = Console.ReadLine();
+                Console.WriteLine("Mi legyen az értékelése?");
+                float score = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Mi legyen az ára?");
+                int price = Convert.ToInt32(Console.ReadLine());
+
+                connection.Create(name, score, price);
+            }
+            else if (input == "nézelődni" || input == "2")
+            {
+                foreach (string item in await connection.All())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else if (input == "törölni" || input == "3")
+            {
+                Console.WriteLine("Mi legyen az id-ja?");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                connection.Delete(id);
+            }
+            else
+            {
+                Console.WriteLine("Hejtelen kommand");
+            }
+        }
+    }
+}
